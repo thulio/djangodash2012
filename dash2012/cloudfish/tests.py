20d991f91672a1ed09f22bbea14ccab4fd4244c7
Thúlio Costa <thulio@thuliocosta.com>
2012-08-18 19:57:30 -0300

    Verify if an email is already in use

--- dash2012/cloudfish/tests.py
+++ dash2012/cloudfish/tests.py
@@ -1,16 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from cloudfish.models import Cloud
 from cloudfish import CLOUD_RACKSPACE
 from auth.models import Account
 from django.contrib.auth.models import User
 from django.core.signing import BadSignature
+from django.test.client import Client
 
 
 class KeyEncriptiontest(TestCase):
@@ -27,6 +21,20 @@ class KeyEncriptiontest(TestCase):
 
         _data = cloud.add_auth_data(salt='mypassword', **auth_data)
         self.assertEquals(_data, cloud.auth_data)
-        self.assertRaises(BadSignature, cloud.decode_auth_data, salt='worng-salt')
+        self.assertRaises(BadSignature, cloud.decode_auth_data,
+            salt='worng-salt')
         self.assertEquals(auth_data, cloud.decode_auth_data(salt='mypassword'))
+
+    def test_validate_email(self):
+        """
+        Tests if an email is alredy in use
+        """
+        user = User(email="daltonmatos@email.com", password="mypassword")
+        user.save()
+        client = Client()
+        response = client.post("/register",
+                {"email": user.email, "password": user.password,
+                 "confirm-password": user.password})
+
+        self.assertIn("This email is already in use.", response.content)
 
--- dash2012/cloudfish/views.py
+++ dash2012/cloudfish/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_protect
 
 
 def index(request):
@@ -15,20 +15,27 @@ def index(request):
 def account(request):
     return render(request, 'account.html', {'active_account': 'active'})
 
+
 @login_required
 def servers(request):
     return render(request, 'servers.html', {'active_servers': 'active'})
 
+@csrf_protect
 def register(request):
+    c = {}
     if request.POST:
-        #name = request.POST['name']
-        username = request.POST['email']
+        username = email = request.POST['email']
         password = request.POST['password']
-        confirm = request.POST['password']
-        email = request.POST['email']
+        confirm = request.POST['confirm-password']
+
+        if User.objects.filter(email=username):
+            c["errors"] = "This email is already in use."
+            return render(request, "register.html", c)
+
         new_user = User.objects.create_user(username, email, password)
         new_user.save()
 
         user = authenticate(username=username, password=password)
         login(request, user)
-    return render(request, 'register.html')
+
+    return render(request, 'register.html', c)
